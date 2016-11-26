f9b052ba7e261d1f7aebfe9a814204ff3fb190e8
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2016-11-26 14:00:52 +0100

    compatibility to smac

--- hpolib/benchmarks/synthetic_functions/camelback.py
+++ hpolib/benchmarks/synthetic_functions/camelback.py
@@ -7,13 +7,13 @@ from hpolib.abstract_benchmark import AbstractBenchmark
 class Camelback(AbstractBenchmark):
     @AbstractBenchmark._check_configuration
     @AbstractBenchmark._configuration_as_array
-    def objective_function(self, x):
+    def objective_function(self, x, **kwargs):
         y = (4 - 2.1 * (x[0] ** 2) + ((x[0] ** 4) / 3)) * (x[0] ** 2) + x[0] * x[1] + (-4 + 4 * (
         x[1] ** 2)) * (x[1] ** 2)
 
         return {'function_value': y}
 
-    def objective_function_test(self, x):
+    def objective_function_test(self, x, **kwargs):
         return self.objective_function(x)
 
     @staticmethod
