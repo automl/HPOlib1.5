c6a24df7150a68b1eb73b92a04e68ca56a45aecb
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2016-12-01 16:59:07 +0100

    ADD config file

--- hpolib/__init__.py
+++ hpolib/__init__.py
@@ -2,4 +2,5 @@ __authors__ = ["Katharina Eggensperger", "Matthias Feurer", "Aaron Klein",
                "Stefan Falkner"]
 __contact__ = "automl.org"
 
-__version__ = "0.0.1"
+from hpolib.__version__ import __version__
+import hpolib.config
