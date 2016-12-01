fab45720caf4f3faa30f2fd6def8b5f516c35794
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2016-12-01 16:58:23 +0100

    FIX

---
+++ hpolib/__init__.py
@@ -0,0 +1,5 @@
+__authors__ = ["Katharina Eggensperger", "Matthias Feurer", "Aaron Klein",
+               "Stefan Falkner"]
+__contact__ = "automl.org"
+
+from hpolib.__version__ import __version__
