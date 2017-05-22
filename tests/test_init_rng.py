84b3827d3fdfe8741858f9fb1d0a0d7c6b0e7d17
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2017-05-22 18:41:28 +0200

    MAINT also check test obj func

--- tests/test_init_rng.py
+++ tests/test_init_rng.py
@@ -8,7 +8,9 @@ import unittest
 import unittest.mock
 
 import hpolib
+#import hpolib.util.rng_helper
 import hpolib.benchmarks.ml
+
 from hpolib.abstract_benchmark import AbstractBenchmark
 
 import numpy as np
@@ -16,7 +18,9 @@ import numpy as np
 
 class TestInitRng(unittest.TestCase):
 
-    def test_init_rng(self):
+    @unittest.mock.patch('hpolib.util.rng_helper.get_rng')
+    def test_init_rng(self, rng_mock):
+        rng_mock.side_effect = Exception("Used get_rng")
         path = os.path.dirname(hpolib.benchmarks.ml.__file__)
 
         for _, pkg, _ in pkgutil.iter_modules([path, ]):
@@ -28,20 +32,39 @@ class TestInitRng(unittest.TestCase):
             abstract_class = None
             for name, obj in inspect.getmembers(mod_name,
                                                 inspect.isclass):
-                if issubclass(obj, AbstractBenchmark) and inspect.isclass(obj):
+                if issubclass(obj, AbstractBenchmark) and \
+                        inspect.isclass(obj) and \
+                        AbstractBenchmark in obj.__bases__:
                     abstract_class = obj
 
             # Check whether all classes accept rng
             if abstract_class is not None:
                 for name, obj in inspect.getmembers(mod_name,
                                                 inspect.isclass):
-                    if issubclass(obj, abstract_class) and inspect.isclass(obj):
-                        print(name)
+                    if issubclass(obj, abstract_class) and \
+                            inspect.isclass(obj) and \
+                            abstract_class in obj.__bases__:
+                        print(obj)
+
                         rng = np.random.RandomState(1)
                         b = getattr(mod_name, name)(rng=rng)
-                        self.assertEqual(b.rng, rng)
+                        self.assertListEqual(list(b.rng.get_state()[1]),
+                                             list(rng.get_state()[1]))
 
-                        b = getattr(mod_name, name)(rng=2)
-                        self.assertEqual(b.rng.get_state()[1], rng.get_state()[1])
+                        b = getattr(mod_name, name)(rng=1)
+                        rng = np.random.RandomState(1)
+                        self.assertListEqual(list(b.rng.get_state()[1]),
+                                             list(rng.get_state()[1]))
+
+                        cs = b.get_configuration_space()
+                        cfg = cs.sample_configuration()
+                        self.assertRaisesRegex(Exception, "Used get_rng",
+                                               b.objective_function,
+                                               cfg, seed=1)
+
+                        self.assertRaisesRegex(Exception, "Used get_rng",
+                                               b.objective_function_test,
+                                               cfg, seed=1)
+
 
 
