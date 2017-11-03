debad7ed75264a710a2832a93ea23cebd0248cdf
Stefan Falkner <sfalkner@cs.uni-freiburg.de>
2017-11-03 11:25:55 +0100

    added a restricted ParamNet searchspace to emulate the constant parametrization

--- hpolib/benchmarks/ml/surrogate_paramnet.py
+++ hpolib/benchmarks/ml/surrogate_paramnet.py
@@ -89,6 +89,100 @@ class SurrogateParamNet(AbstractBenchmark):
                 }
 
 
+
+class SurrogateReducedParamNetTime(AbstractBenchmark):
+
+    def __init__(self, dataset, path=None, rng=None):
+        super(SurrogateReducedParamNetTime, self).__init__()
+        self.surrogate_objective = pickle.load(open(os.path.join(path, "rf_surrogate_paramnet_%s.pkl" % dataset), "rb"))
+        self.surrogate_cost = pickle.load(open(os.path.join(path, "rf_cost_surrogate_paramnet_%s.pkl" % dataset), "rb"))
+        self.n_epochs = 50
+        self.dataset = dataset
+        if rng is None:
+            self.rng = np.random.RandomState()
+        else:
+            self.rng = rng
+
+    @AbstractBenchmark._check_configuration
+    @AbstractBenchmark._configuration_as_array
+    def objective_function(self, x, budget=None, **kwargs):
+        # If no budget is specified we train this config for the max number of epochs
+        if budget is None:
+            budget = np.inf
+
+        x_ = np.zeros([1,8], dtype=np.float)
+        
+        x_[0,0] = 10 ** x[0]
+        x_[0,1] = 2 ** x[1]
+        x_[0,2] = 2 ** x[2]
+        x_[0,3] = 10 ** x[3]
+        x_[0,4] = 0.5
+        x_[0,5] = x[4]
+        x_[0,6] = x[5]
+        x_[0,7] = x[5]
+
+        
+        
+        lc = self.surrogate_objective.predict(x_)[0]
+        c = self.surrogate_cost.predict(x_)[0]
+
+        # Check if we can afford a single epoch in the budget
+        if c / self.n_epochs > budget:
+            # TODO: Return random performance here instead
+            y = 1
+            return {'function_value': y, "cost": budget}
+
+        learning_curves_cost = np.linspace(c / self.n_epochs, c, self.n_epochs)
+
+        idx = np.where(learning_curves_cost < budget)[0][-1]
+        y = lc[idx]
+
+        return {'function_value': y, "cost": budget}
+
+    @AbstractBenchmark._check_configuration
+    @AbstractBenchmark._configuration_as_array
+    def objective_function_test(self, x, **kwargs):
+        x_ = deepcopy(x)
+        x_[0] = 10 ** x_[0]
+        x_[1] = 2 ** x_[1]
+        x_[2] = 2 ** x_[2]
+        x_[3] = 10 ** x_[3]
+        lc = self.surrogate_objective.predict(x_)[0]
+        c = self.surrogate_cost.predict(x_)[0]
+        y = lc[-1]
+        return {'function_value': y, "cost": c}
+
+    @staticmethod
+    def get_configuration_space():
+        cs = CS.ConfigurationSpace()
+        cs.generate_all_continuous_from_bounds(SurrogateReducedParamNetTime.get_meta_information()['bounds'])
+        return cs
+
+    @staticmethod
+    def get_meta_information():
+        return {'name': 'Stefans reparameterization of paramnet',
+                # 'bounds': [[1e-6, 1e-2],  # initial_lr
+                #            [8, 256],  # batch_size
+                #            [16, 256],  # average_units_per_layer
+                #            [1e-4, 1],  # final_lr_fraction
+                #            [0, 1.],  # shape_parameter_1
+                #            [1, 5],  # num_layers
+                #            [0, .5],  # dropout_0
+                #            [0, .5]]  # dropout_1
+                'bounds': [[-6, -2],  # log10 initial_lr
+                           [3, 8],  # log2 batch_size
+                           [4, 8],  # log2 average_units_per_layer
+                           [-4, 0],  # log10 final_lr_fraction
+                           #[0, 1.],  # shape_parameter_1
+                           [1, 5],  # num_layers
+                           #[0, .5]  # dropout_0
+                           [0, .5]]  # dropout_1
+                }
+
+
+
+
+
 class SurrogateParamNetTime(SurrogateParamNet):
 
     @AbstractBenchmark._check_configuration
