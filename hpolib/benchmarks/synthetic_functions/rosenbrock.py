6c0714d33e8d17a6ed8ec65b5c3a6e4e395bf226
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2018-01-29 15:44:05 +0100

    add multi fidelity rosenbrock

--- hpolib/benchmarks/synthetic_functions/rosenbrock.py
+++ hpolib/benchmarks/synthetic_functions/rosenbrock.py
@@ -1,14 +1,20 @@
 import ConfigSpace as CS
+import numpy as np
+
 from hpolib.abstract_benchmark import AbstractBenchmark
 
 
 class Rosenbrock(AbstractBenchmark):
+
+    def __init__(self, d=2, **kwargs):
+        self.d = d
+        super(Rosenbrock, self).__init__(**kwargs)
+
     @AbstractBenchmark._check_configuration
     @AbstractBenchmark._configuration_as_array
     def objective_function(self, x, **kwargs):
         y = 0
-        d = 2
-        for i in range(d - 1):
+        for i in range(self.d - 1):
             y += 100 * (x[i + 1] - x[i] ** 2) ** 2
             y += (x[i] - 1) ** 2
 
@@ -17,106 +23,39 @@ class Rosenbrock(AbstractBenchmark):
     def objective_function_test(self, x, **kwargs):
         return self.objective_function(x)
 
-    @staticmethod
-    def get_configuration_space():
+    def get_configuration_space(self):
         cs = CS.ConfigurationSpace()
-        cs.generate_all_continuous_from_bounds(Rosenbrock.get_meta_information()['bounds'])
+        cs.generate_all_continuous_from_bounds(self.get_meta_information()['bounds'])
         return cs
 
-    @staticmethod
-    def get_meta_information():
+    def get_meta_information(self):
         return {'name': 'Rosenbrock',
-                'num_function_evals': 200,
-                'optima': ([[1, 1]]),
-                'bounds': [[-5, 10], [-5, 10]],
+                'num_function_evals': self.d * 100,
+                'optima': ([[1] * self.d]),
+                'bounds': [[-5, 10]] * self.d,
                 'f_opt': 0.0}
 
 
-class Rosenbrock5D(AbstractBenchmark):
+class MultiFidelityRosenbrock(Rosenbrock):
+
     @AbstractBenchmark._check_configuration
     @AbstractBenchmark._configuration_as_array
-    def objective_function(self, x, **kwargs):
+    def objective_function(self, x, budget=100, **kwargs):
+        #shift = (10 + 2 * np.log(budget / 100.)) / 10.
+
+        #f_bias = np.log(100) - np.log(budget)
+        f_bias = 0
+        shift = 2 - 2 * (budget / 100)
         y = 0
-        d = 5
-        for i in range(d - 1):
-            y += 100 * (x[i + 1] - x[i] ** 2) ** 2
-            y += (x[i] - 1) ** 2
+        for i in range(self.d - 1):
+            zi = x[i] - shift
+            zi_next = x[i + 1] - shift
+
+            y += 100 * (zi_next - zi ** 2) ** 2
+            y += (zi - 1) ** 2
+            y += f_bias
 
         return {'function_value': y}
 
     def objective_function_test(self, x, **kwargs):
-        return self.objective_function(x)
-
-    @staticmethod
-    def get_configuration_space():
-        cs = CS.ConfigurationSpace()
-        cs.generate_all_continuous_from_bounds(Rosenbrock5D.get_meta_information()['bounds'])
-        return cs
-
-    @staticmethod
-    def get_meta_information():
-        return {'name': 'Rosenbrock5D',
-                'num_function_evals': 500,
-                'optima': ([[1] * 5]),
-                'bounds': [[-5, 10]] * 5,
-                'f_opt': 0.0}
-
-
-class Rosenbrock10D(AbstractBenchmark):
-    @AbstractBenchmark._check_configuration
-    @AbstractBenchmark._configuration_as_array
-    def objective_function(self, x, **kwargs):
-        y = 0
-        d = 10
-        for i in range(d - 1):
-            y += 100 * (x[i + 1] - x[i] ** 2) ** 2
-            y += (x[i] - 1) ** 2
-
-        return {'function_value': y}
-
-    def objective_function_test(self, x, **kwargs):
-        return self.objective_function(x)
-
-    @staticmethod
-    def get_configuration_space():
-        cs = CS.ConfigurationSpace()
-        cs.generate_all_continuous_from_bounds(Rosenbrock10D.get_meta_information()['bounds'])
-        return cs
-
-    @staticmethod
-    def get_meta_information():
-        return {'name': 'Rosenbrock10D',
-                'num_function_evals': 1000,
-                'optima': ([[1] * 10]),
-                'bounds': [[-5, 10]] * 10,
-                'f_opt': 0.0}
-
-
-class Rosenbrock20D(AbstractBenchmark):
-    @AbstractBenchmark._check_configuration
-    @AbstractBenchmark._configuration_as_array
-    def objective_function(self, x, **kwargs):
-        y = 0
-        d = 20
-        for i in range(d - 1):
-            y += 100 * (x[i + 1] - x[i] ** 2) ** 2
-            y += (x[i] - 1) ** 2
-
-        return {'function_value': y}
-
-    def objective_function_test(self, x, **kwargs):
-        return self.objective_function(x)
-
-    @staticmethod
-    def get_configuration_space():
-        cs = CS.ConfigurationSpace()
-        cs.generate_all_continuous_from_bounds(Rosenbrock20D.get_meta_information()['bounds'])
-        return cs
-
-    @staticmethod
-    def get_meta_information():
-        return {'name': 'Rosenbrock20D',
-                'num_function_evals': 2000,
-                'optima': ([[1] * 20]),
-                'bounds': [[-5, 10]] * 20,
-                'f_opt': 0.0}
+        return self.objective_function(x, budget=100)
