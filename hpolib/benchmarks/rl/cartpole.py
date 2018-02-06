eda4c867727b06b0ff901c2e71052f7299028a59
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2018-02-06 14:57:28 +0100

    add continous version

--- hpolib/benchmarks/rl/cartpole.py
+++ hpolib/benchmarks/rl/cartpole.py
@@ -69,7 +69,6 @@ class Cartpole(AbstractBenchmark):
                 discount=config["discount"],
                 # DistributionModel
                 distributions_spec=None,
-                entropy_regularization=config["entropy_regularization"],
                 # PGModel
                 baseline_mode=config["baseline_mode"],
                 baseline={
@@ -84,6 +83,165 @@ class Cartpole(AbstractBenchmark):
                     },
                     "num_steps": config["baseline_optimization_steps"]
                 },
+                gae_lambda=None,
+                # PGLRModel
+                likelihood_ratio_clipping=config["likelihood_ratio_clipping"],
+                summary_spec=None,
+                distributed_spec=None
+            )
+
+            def episode_finished(r):
+                # Check if we have converged
+                print(r.episode_rewards[-1])
+                if np.mean(r.episode_rewards[-self.avg_n_episodes:]) == 200:
+                    return False
+                else:
+                    return True
+
+            runner = Runner(agent=agent, environment=self.env)
+
+            runner.run(episodes=self.max_episodes, max_episode_timesteps=200, episode_finished=episode_finished)
+
+            converged_episodes.append(len(runner.episode_rewards))
+
+        cost = time.time() - st
+
+        return {'function_value': np.mean(converged_episodes), "cost": cost, "all_runs": converged_episodes}
+
+    @AbstractBenchmark._check_configuration
+    def objective_function_test(self, config, **kwargs):
+
+        return self.objective_function(self, config, budget=self.max_budget)
+
+    @staticmethod
+    def get_configuration_space():
+        cs = CS.ConfigurationSpace()
+
+        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("n_units_1",
+                                                              lower=8,
+                                                              default_value=64,
+                                                              upper=64,
+                                                              log=True))
+
+        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("n_units_2",
+                                                              lower=8,
+                                                              default_value=64,
+                                                              upper=64,
+                                                              log=True))
+
+        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("batch_size",
+                                                              lower=8,
+                                                              default_value=64,
+                                                              upper=256,
+                                                              log=True))
+
+        cs.add_hyperparameter(CS.UniformFloatHyperparameter("learning_rate",
+                                                            lower=1e-7,
+                                                            default_value=1e-3,
+                                                            upper=1e-1,
+                                                            log=True))
+
+        cs.add_hyperparameter(CS.UniformFloatHyperparameter("discount",
+                                                            lower=0,
+                                                            default_value=.99,
+                                                            upper=1))
+
+        cs.add_hyperparameter(CS.UniformFloatHyperparameter("likelihood_ratio_clipping",
+                                                            lower=0,
+                                                            default_value=.2,
+                                                            upper=1))
+
+        cs.add_hyperparameter(CS.CategoricalHyperparameter("activation_1", ["tanh", "relu"]))
+
+        cs.add_hyperparameter(CS.CategoricalHyperparameter("activation_2", ["tanh", "relu"]))
+
+        cs.add_hyperparameter(CS.CategoricalHyperparameter("optimizer_type", ["adam", "rmsprop"]))
+
+        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("optimization_steps",
+                                                              lower=1,
+                                                              default_value=10,
+                                                              upper=10))
+
+        cs.add_hyperparameter(CS.CategoricalHyperparameter("baseline_mode", ["states", "network"]))
+
+        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("baseline_n_units_1",
+                                                              lower=8,
+                                                              default_value=64,
+                                                              upper=128,
+                                                              log=True))
+
+        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("baseline_n_units_2",
+                                                              lower=8,
+                                                              default_value=64,
+                                                              upper=128,
+                                                              log=True))
+
+        cs.add_hyperparameter(CS.UniformFloatHyperparameter("baseline_learning_rate",
+                                                            lower=1e-7,
+                                                            default_value=1e-3,
+                                                            upper=1e-1,
+                                                            log=True))
+
+        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("baseline_optimization_steps",
+                                                              lower=1,
+                                                              default_value=10,
+                                                              upper=10))
+
+        cs.add_hyperparameter(CS.CategoricalHyperparameter("baseline_optimizer_type", ["adam", "rmsprop"]))
+        return cs
+
+    @staticmethod
+    def get_meta_information():
+        return {'name': 'Cartpole',
+                'references': []
+                }
+
+
+class ContinuousCartpole(Cartpole):
+
+    @AbstractBenchmark._check_configuration
+    def objective_function(self, config, budget=None, **kwargs):
+
+        st = time.time()
+
+        if budget is None:
+            budget = self.max_budget
+
+        network_spec = [
+            dict(type='dense', size=config["n_units_1"], activation='tanh'),
+            dict(type='dense', size=config["n_units_2"], activation='tanh')
+        ]
+
+        converged_episodes = []
+
+        for i in range(budget):
+            agent = PPOAgent(
+                states_spec=self.env.states,
+                actions_spec=self.env.actions,
+                network_spec=network_spec,
+                batch_size=config["batch_size"],
+                # Agent
+                states_preprocessing_spec=None,
+                explorations_spec=None,
+                reward_preprocessing_spec=None,
+                # BatchAgent
+                keep_last_timestep=True,
+                # PPOAgent
+                step_optimizer=dict(
+                    type='adam',
+                    learning_rate=config["learning_rate"]
+                ),
+                optimization_steps=10,
+                # Model
+                scope='ppo',
+                discount=config["discount"],
+                # DistributionModel
+                distributions_spec=None,
+                entropy_regularization=config["entropy_regularization"],
+                # PGModel
+                baseline_mode=None,
+                baseline=None,
+                baseline_optimizer=None,
                 gae_lambda=None,
                 # PGLRModel
                 likelihood_ratio_clipping=config["likelihood_ratio_clipping"],
@@ -129,6 +287,12 @@ class Cartpole(AbstractBenchmark):
                                                               upper=128,
                                                               log=True))
 
+        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("n_units_3",
+                                                              lower=8,
+                                                              default_value=64,
+                                                              upper=128,
+                                                              log=True))
+
         cs.add_hyperparameter(CS.UniformIntegerHyperparameter("batch_size",
                                                               lower=8,
                                                               default_value=64,
@@ -151,47 +315,8 @@ class Cartpole(AbstractBenchmark):
                                                             default_value=.2,
                                                             upper=1))
 
-        cs.add_hyperparameter(CS.CategoricalHyperparameter("activation_1", ["tanh", "relu"]))
-
-        cs.add_hyperparameter(CS.CategoricalHyperparameter("activation_2", ["tanh", "relu"]))
-
-        cs.add_hyperparameter(CS.CategoricalHyperparameter("optimizer_type", ["adam", "momentum", "rmsprop"]))
-
-        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("optimization_steps",
-                                                              lower=1,
-                                                              default_value=10,
-                                                              upper=100))
-
-        cs.add_hyperparameter(CS.CategoricalHyperparameter("baseline_mode", ["states", "network"]))
-
-        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("baseline_n_units_1",
-                                                              lower=8,
-                                                              default_value=64,
-                                                              upper=128,
-                                                              log=True))
-
-        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("baseline_n_units_2",
-                                                              lower=8,
-                                                              default_value=64,
-                                                              upper=128,
-                                                              log=True))
-
-        cs.add_hyperparameter(CS.UniformFloatHyperparameter("baseline_learning_rate",
-                                                            lower=1e-7,
-                                                            default_value=1e-3,
-                                                            upper=1e-1,
-                                                            log=True))
-
-        cs.add_hyperparameter(CS.UniformIntegerHyperparameter("baseline_optimization_steps",
-                                                              lower=1,
-                                                              default_value=10,
-                                                              upper=100))
-
-        cs.add_hyperparameter(CS.CategoricalHyperparameter("baseline_optimizer_type", ["adam", "momentum", "rmsprop"]))
+        cs.add_hyperparameter(CS.UniformFloatHyperparameter("entropy_regularization",
+                                                            lower=0,
+                                                            default_value=0.01,
+                                                            upper=1))
         return cs
-
-    @staticmethod
-    def get_meta_information():
-        return {'name': 'Cartpole',
-                'references': []
-                }
