8436143baa718f8e00563765bb388a211aa9992b
Stefan Falkner <sfalkner@cs.uni-freiburg.de>
2018-07-17 09:58:13 +0200

    added extras_require to setup.py and cleaned up some requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,15 @@ setuptools.setup(
     description='Automated machine learning.',
     version=version,
     packages=setuptools.find_packages(exclude=['test']),
-    install_requires=[],
+    install_requires=['cython', 'numpy>=1.9.0', 'scipy>=0.14.1', 'ConfigSpace>=0.4.0', 'configparser', 'matplotlib'],
+    extras_require={
+        'BNNs':         ['theano', 'git+https://github.com/Lasagne/Lasagne.git@master', 'git+https://github.com/automl/sgmcmc.git'],
+        'testing':      ['unittest2', 'nose'],
+        'auto-sklearn': ['auto-sklearn==0.3.0', 'pynisher'],
+        '?':            ['scikit-learn>=0.19.0,<0.20.0'],
+        '??':           ['openml'],
+        
+    }
     test_suite='nose.collector',
     scripts=[],
     #include_package_data=True,
