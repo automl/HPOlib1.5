ea0cea678537ed30a8d796a24d4539ec4aae4b73
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2018-08-16 20:53:15 +0200

    fix travis

--- setup.py
+++ setup.py
@@ -4,12 +4,22 @@ import setuptools
 with open("hpolib/__version__.py") as fh:
     version = fh.readlines()[-1].split()[-1].strip("\"'")
 
+with open('requirements.txt') as fh:
+    requirements = fh.read()
+requirements = requirements.split('\n')
+requirements = [requirement.strip() for requirement in requirements]
+
+
 setuptools.setup(
     name='hpolib2',
     description='Automated machine learning.',
     version=version,
     packages=setuptools.find_packages(exclude=['test']),
-    install_requires=[],
+    install_requires=requirements,
+    tests_require=[
+        'pynisher',
+        'nose',
+        'mock'],
     test_suite='nose.collector',
     scripts=[],
     #include_package_data=True,
