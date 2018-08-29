4e5793fc2308768b18ee10150c490e2638ed8e8d
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2018-08-29 11:13:09 +0200

    Adjust synth functions (#56)
    * 100 evals for 1D, 200 for everything else; Also rename Levy1D/Rosenbrock2D
    
    * fix

--- hpolib/benchmarks/synthetic_functions/__init__.py
+++ hpolib/benchmarks/synthetic_functions/__init__.py
@@ -2,11 +2,11 @@ from hpolib.benchmarks.synthetic_functions.branin import Branin
 from hpolib.benchmarks.synthetic_functions.hartmann3 import Hartmann3
 from hpolib.benchmarks.synthetic_functions.hartmann6 import Hartmann6
 from hpolib.benchmarks.synthetic_functions.camelback import Camelback
-from hpolib.benchmarks.synthetic_functions.levy import Levy
+from hpolib.benchmarks.synthetic_functions.levy import Levy1D
 from hpolib.benchmarks.synthetic_functions.bohachevsky import Bohachevsky
 from hpolib.benchmarks.synthetic_functions.sin_one import SinOne
 from hpolib.benchmarks.synthetic_functions.sin_two import SinTwo
 from hpolib.benchmarks.synthetic_functions.goldstein_price import GoldsteinPrice
-from hpolib.benchmarks.synthetic_functions.rosenbrock import Rosenbrock
+from hpolib.benchmarks.synthetic_functions.rosenbrock import Rosenbrock2D
 from hpolib.benchmarks.synthetic_functions.forrester import Forrester
 from hpolib.benchmarks.synthetic_functions.wrapper.synthetic_noise_and_cost import SyntheticNoiseAndCost
