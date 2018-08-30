9f73a268e9731dcf23eb24e17e7490610c4f6d80
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2018-08-30 16:59:34 +0200

    test length of bounds

--- tests/test_synthetic_functions.py
+++ tests/test_synthetic_functions.py
@@ -1,65 +1,57 @@
 import unittest
-import numpy as np
 
 from hpolib.benchmarks import synthetic_functions
 
 
 class TestAbstractBenchmark(unittest.TestCase):
 
+    def _test_synth_function(self, f):
+        for x in f.get_meta_information()["optima"]:
+            self.assertAlmostEqual(f(x), f.get_meta_information()["f_opt"], places=7)
+
+        for x in f.get_meta_information()['bounds']:
+            self.assertEqual(len(x), 2)
+
+        self.assertEqual(len(f.get_meta_information()['bounds']),
+                         len(f.get_configuration_space().get_hyperparameters()))
+
     def test_branin(self):
         f = synthetic_functions.Branin()
-
-        for x in f.get_meta_information()["optima"]:
-            np.testing.assert_approx_equal(f(x), f.get_meta_information()["f_opt"], significant=9)
+        self._test_synth_function(f)
 
     def test_hartmann3(self):
         f = synthetic_functions.Hartmann3()
-
-        for x in f.get_meta_information()["optima"]:
-            np.testing.assert_approx_equal(f(x), f.get_meta_information()["f_opt"], significant=9)
+        self._test_synth_function(f)
 
     def test_hartmann6(self):
         f = synthetic_functions.Hartmann6()
-
-        for x in f.get_meta_information()["optima"]:
-            np.testing.assert_approx_equal(f(x), f.get_meta_information()["f_opt"], significant=9)
+        self._test_synth_function(f)
 
     def test_camelback(self):
         f = synthetic_functions.Camelback()
-
-        for x in f.get_meta_information()["optima"]:
-            np.testing.assert_approx_equal(f(x), f.get_meta_information()["f_opt"], significant=9)
+        self._test_synth_function(f)
 
     def test_levy(self):
         for i in range(1, 11):
             f = getattr(synthetic_functions, "Levy%dD" % i)()
-
-            for x in f.get_meta_information()["optima"]:
-                assert np.isclose(f(x), f.get_meta_information()["f_opt"])
+            self._test_synth_function(f)
 
     def test_goldstein_price(self):
         f = synthetic_functions.GoldsteinPrice()
-
-        for x in f.get_meta_information()["optima"]:
-            assert np.isclose(f(x), f.get_meta_information()["f_opt"])
+        self._test_synth_function(f)
 
     def test_rosenbrock(self):
-        f = synthetic_functions.Rosenbrock2D()
-
-        for x in f.get_meta_information()["optima"]:
-            np.testing.assert_approx_equal(f(x), f.get_meta_information()["f_opt"], significant=9)
+        for i in range(2, 5, 10):
+            f = getattr(synthetic_functions, "Rosenbrock%dD" % i)()
+            self._test_synth_function(f)
 
     def test_sin_one(self):
         f = synthetic_functions.SinOne()
-
-        for x in f.get_meta_information()["optima"]:
-            np.testing.assert_approx_equal(f(x), f.get_meta_information()["f_opt"], significant=9)
+        self._test_synth_function(f)
 
     def test_sin_two(self):
         f = synthetic_functions.SinTwo()
-
-        for x in f.get_meta_information()["optima"]:
-            np.testing.assert_approx_equal(f(x), f.get_meta_information()["f_opt"], significant=9)
+        self._test_synth_function(f)
 
 if __name__ == "__main__":
     unittest.main()
