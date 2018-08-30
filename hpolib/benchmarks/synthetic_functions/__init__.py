29172335ea7154d8fbccbda5319775d62e5b83d2
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2018-08-30 11:22:20 +0200

    increase number of levy dimensions

--- hpolib/benchmarks/synthetic_functions/__init__.py
+++ hpolib/benchmarks/synthetic_functions/__init__.py
@@ -2,11 +2,11 @@ from hpolib.benchmarks.synthetic_functions.branin import Branin
 from hpolib.benchmarks.synthetic_functions.hartmann3 import Hartmann3
 from hpolib.benchmarks.synthetic_functions.hartmann6 import Hartmann6
 from hpolib.benchmarks.synthetic_functions.camelback import Camelback
-from hpolib.benchmarks.synthetic_functions.levy import Levy1D
+from hpolib.benchmarks.synthetic_functions.levy import Levy1D, Levy2D, Levy3D, Levy4D, Levy5D, Levy6D, Levy7D, Levy8D, Levy9D, Levy10D
 from hpolib.benchmarks.synthetic_functions.bohachevsky import Bohachevsky
 from hpolib.benchmarks.synthetic_functions.sin_one import SinOne
 from hpolib.benchmarks.synthetic_functions.sin_two import SinTwo
 from hpolib.benchmarks.synthetic_functions.goldstein_price import GoldsteinPrice
-from hpolib.benchmarks.synthetic_functions.rosenbrock import Rosenbrock2D
+from hpolib.benchmarks.synthetic_functions.rosenbrock import Rosenbrock2D, Rosenbrock5D, Rosenbrock10D, Rosenbrock20D
 from hpolib.benchmarks.synthetic_functions.forrester import Forrester
 from hpolib.benchmarks.synthetic_functions.wrapper.synthetic_noise_and_cost import SyntheticNoiseAndCost
