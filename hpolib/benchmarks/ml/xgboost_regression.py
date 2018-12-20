c574abc2b575c424813b327f0d01387b34d7f4f3
Aaron Klein <kleinaa@cs.uni-freiburg.de>
2018-12-20 16:37:53 +0100

    switch hps

--- hpolib/benchmarks/ml/xgboost_regression.py
+++ hpolib/benchmarks/ml/xgboost_regression.py
@@ -80,10 +80,10 @@ class XGBoostRegressionUCI(AbstractBenchmark):
                                      gamma=configuration["gamma"],
                                      reg_alpha=configuration["reg_alpha"],
                                      reg_lambda=configuration["reg_lambda"],
-                                     colsample_bylevel=configuration["colsample_bylevel"],
                                      colsample_bytree=configuration["colsample_bytree"],
                                      subsample=configuration["subsample"],
-                                     max_depth=configuration["max_depth"])
+                                     max_depth=configuration["max_depth"],
+                                     min_child_weight=configuration["min_child_weight"])
 
         xgb_model.fit(self.train, self.train_targets)
 
@@ -107,9 +107,9 @@ class XGBoostRegressionUCI(AbstractBenchmark):
                                      reg_alpha=configuration["reg_alpha"],
                                      reg_lambda=configuration["reg_lambda"],
                                      colsample_bylevel=configuration["colsample_bylevel"],
-                                     colsample_bytree=configuration["colsample_bytree"],
                                      subsample=configuration["subsample"],
-                                     max_depth=configuration["max_depth"])
+                                     max_depth=configuration["max_depth"],
+                                     min_child_weight=configuration["min_child_weight"])
 
         xgb_model.fit(train, train_targets)
 
@@ -139,9 +139,6 @@ class XGBoostRegressionUCI(AbstractBenchmark):
             'reg_lambda', lower=1e-5, upper=1e3, log=True))
 
         cs.add_hyperparameter(ConfigSpace.UniformFloatHyperparameter(
-            'colsample_bylevel', lower=1e-2, upper=1, log=True))
-
-        cs.add_hyperparameter(ConfigSpace.UniformFloatHyperparameter(
             'colsample_bytree', lower=1e-2, upper=1, log=True))
 
         cs.add_hyperparameter(ConfigSpace.UniformFloatHyperparameter(
@@ -149,6 +146,10 @@ class XGBoostRegressionUCI(AbstractBenchmark):
 
         cs.add_hyperparameter(ConfigSpace.UniformIntegerHyperparameter(
             'max_depth', lower=1, upper=15))
+
+        cs.add_hyperparameter(ConfigSpace.UniformIntegerHyperparameter(
+            'min_child_weight', lower=0, upper=20))
+
         return cs
 
     @staticmethod
